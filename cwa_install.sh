b398a44acba32c2efa8cde34e9aa4809508cd62a
vhsdream <punk.sand7393@fastmail.com>
2025-02-25 16:45:34 -0500

    clean up install script

--- cwa_install.sh
+++ cwa_install.sh
@@ -1,12 +1,24 @@
 #! /usr/bin/env bash
 
-# dirty install script for Calibre-Web Automated
-# done over top of a Calibre-Web install via Proxmox Helper Script
-# Author: vhsdream
+# Dirty install script for Calibre-Web Automated
+# Done over top of a Calibre-Web install via Proxmox Helper Script
+# https://github.com/community-scripts/ProxmoxVE/blob/main/install/calibre-web-install.sh
+# Author: vhsdream 2025
+# USE AT YOUR OWN RISK
+# AFTER RUNNING THIS SCRIPT IT'S LIKELY YOU CAN NO LONGER USE THE 'update' FUNCTION
+# FROM Proxmox Helper Scripts
+
+set -euo pipefail
+
+# give warning
+echo "This script only works if Calibre-Web was installed with the Proxmox Helper Script" && sleep 3
+echo "If it succeeds, you can no longer use the 'update' function from Proxmox Helper Scripts" && sleep 3
+echo "Run this script at your own risk. I TAKE NO RESPONSIBILITY FOR DATA LOSS!" && sleep 3
+echo "You have 5 seconds to cancel before the script proceeds..." && sleep 6
 
 # stop Calibre-web
-systemctl stop cps
-# install deps
+echo "Stopping Calibre-Web service & installing CWA dependencies..." && sleep 1
+systemctl disable -q --now cps
 apt-get update && \
 apt-get install -y --no-install-recommends \
   build-essential \
@@ -24,16 +36,22 @@ apt-get install -y --no-install-recommends \
   inotify-tools \
   sqlite3 \
 
-# get CWA release
+echo "Dependencies installed." && sleep 2
+# get CWA fork with modified scripts
+echo "Downloading CWA from fork..." && sleep 1
 cd /opt
-# RELEASE=$(curl -s https://api.github.com/repos/vhsdream/cwa-lxc/releases/latest | grep "tag_name" | awk '{print substr($2, 3, length($2)-4) }')
-# wget -q "https://github.com/vhsdream/cwa-lxc/archive/refs/tags/V${RELEASE}.zip"
 wget -q "https://github.com/vhsdream/cwa-lxc/archive/refs/heads/dev.zip"
 unzip -q dev.zip
 mv cwa-lxc-dev /opt/cwa
 cd /opt/cwa
 
+# install additional reqs for CWA
+echo "Installing Python requirements for CWA..." && sleep 1
+pip install -r requirements.txt -q
+echo "Installed." && sleep 2
+
 # creating dirs, according to setup-cwa.sh and the cwa-init s6 script (with some changes for compatibility)
+echo "Creating required directories for CWA..." && sleep 1
 mkdir -p /opt/cwa/metadata_change_logs /opt/cwa/metadata_temp /opt/cwa-book-ingest
 mkdir -p /var/lib/cwa/processed_books/converted
 mkdir -p /var/lib/cwa/processed_books/imported
@@ -43,15 +61,12 @@ mkdir -p /var/lib/cwa/log_archive
 mkdir -p /var/lib/cwa/.cwa_conversion_tmp
 # the 'calibre-library' is simply the /opt/calibre-web dir so no need to change
  
-# install additional reqs for CWA
-pip install -r requirements.txt
-
 # copy modified calibre-web files from CWA
+echo "Copying patched Calibre-Web files to local Python lib folder..." && sleep 1
 cp -r /opt/cwa/root/app/calibre-web/cps/* /usr/local/lib/python3.11/dist-packages/calibreweb/cps
 
-# At this point I think a number of systemd service files need to be created for the various CWA workers. It is a TODO.
-#
-
+# Create service files for CWA workers
+echo "Creating CWA worker service files..." && sleep 1
 cat <<EOF >/etc/systemd/system/cwa-autolibrary.service
 [Unit]
 Description=Calibre-Web Automated Auto-Library Service
@@ -102,9 +117,24 @@ Restart=on-failure
 [Install]
 WantedBy=multi-user.target
 EOF
-# Then, maybe after all that is done, we restart the Calibre-web service
-systemctl start cps
-# Then enable the other services
-systemctl enable --now cwa-autolibrary cwa-ingester cwa-change-detector
+
+cat <<EOF >/etc/systemd/system/cwa.target
+[Unit]
+Description=Calibre-Web Automated Services
+After=network-online.target
+Wants=cps.service cwa-autolibrary.service cwa-ingester.service cwa-change-detector.service
+
+[Install]
+WantedBy=multi-user.target
+EOF
+echo "Service files created." && sleep 1
+
+# Enable CWA Service target
+echo "Enabling & starting services via cwa.target..." && sleep 1
+systemctl enable -q --now cwa.target
+echo "CWA started! (hopefully)" && sleep 1
 
 # then cleanup etc etc
+echo "Cleaning up..." && sleep 1
+apt autoremove && apt autoclean
+echo "Done!"
